2f5c859e813b0085d4700081fabd5d6328dcefa2
EgorKhabarov <not.a.fan.of.broccoli@gmail.com>
2025-10-03 22:46:27 +0400

    update

--- src/client/java/egorkhabarov/locator_triangulation/LocatorTriangulationClient.java
+++ src/client/java/egorkhabarov/locator_triangulation/LocatorTriangulationClient.java
@@ -3,13 +3,17 @@ package egorkhabarov.locator_triangulation;
 import egorkhabarov.locator_triangulation.command.LocatorDataCommand;
 import egorkhabarov.locator_triangulation.command.TriangulationCommand;
 import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LocatorTriangulationClient implements ClientModInitializer {
     public static final String MOD_ID = "locator_triangulation";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitializeClient() {
         LocatorDataCommand.register();
         TriangulationCommand.register();
+        LOGGER.info(MOD_ID + " mod loaded successfully!");
     }
 }
