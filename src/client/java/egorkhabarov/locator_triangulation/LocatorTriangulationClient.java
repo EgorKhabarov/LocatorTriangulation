21c929f97ed1f85aa80336ec39cebee2de6b82ce
EgorKhabarov <not.a.fan.of.broccoli@gmail.com>
2025-10-04 23:32:13 +0400

    when entering the world, positions are reset

--- src/client/java/egorkhabarov/locator_triangulation/LocatorTriangulationClient.java
+++ src/client/java/egorkhabarov/locator_triangulation/LocatorTriangulationClient.java
@@ -2,7 +2,10 @@ package egorkhabarov.locator_triangulation;
 
 import egorkhabarov.locator_triangulation.command.LocatorDataCommand;
 import egorkhabarov.locator_triangulation.command.TriangulationCommand;
+import egorkhabarov.locator_triangulation.state.LocatorState;
+import egorkhabarov.locator_triangulation.state.TriangulationState;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,6 +17,14 @@ public class LocatorTriangulationClient implements ClientModInitializer {
     public void onInitializeClient() {
         LocatorDataCommand.register();
         TriangulationCommand.register();
+        ClientPlayConnectionEvents.DISCONNECT.register((handler, client) -> {
+            LocatorState.clearAll();
+            TriangulationState.clearAll();
+        });
+        ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
+            LocatorState.clearAll();
+            TriangulationState.clearAll();
+        });
         LOGGER.info(MOD_ID + " mod loaded successfully!");
     }
 }
