84820f785958b33e02524a4cd7b5b6ae77c6e0b7
EgorKhabarov <not.a.fan.of.broccoli@gmail.com>
2025-10-23 23:13:59 +0400

    add keybinds

--- src/client/java/egorkhabarov/locator_triangulation/command/LocatorDataCommand.java
+++ src/client/java/egorkhabarov/locator_triangulation/command/LocatorDataCommand.java
@@ -1,6 +1,7 @@
 package egorkhabarov.locator_triangulation.command;
 
 import com.mojang.brigadier.arguments.StringArgumentType;
+import egorkhabarov.locator_triangulation.keybinds.Keybinds;
 import egorkhabarov.locator_triangulation.model.Name;
 import egorkhabarov.locator_triangulation.model.LocatorInfo;
 import egorkhabarov.locator_triangulation.model.PlayerInfo;
@@ -35,6 +36,7 @@ public class LocatorDataCommand {
                         ChatUtils.sendErrorMessage("Failed to capture pos1");
                     } else {
                         LocatorState.setPos1(info);
+                        Keybinds.last_first_position = true;
                         ChatUtils.sendConfirmationMessage("Locator pos1 saved");
                     }
                     return 1;
@@ -49,6 +51,7 @@ public class LocatorDataCommand {
                         ChatUtils.sendErrorMessage("Failed to capture pos2");
                     } else {
                         LocatorState.setPos2(info);
+                        Keybinds.last_first_position = false;
                         ChatUtils.sendConfirmationMessage("Locator pos2 saved");
                     }
                     return 1;
---
+++ src/client/java/egorkhabarov/locator_triangulation/keybinds/Keybinds.java
@@ -0,0 +1,71 @@
+package egorkhabarov.locator_triangulation.keybinds;
+
+import egorkhabarov.locator_triangulation.data_providers.LocatorDataProvider;
+import egorkhabarov.locator_triangulation.model.LocatorInfo;
+import egorkhabarov.locator_triangulation.state.LocatorState;
+import egorkhabarov.locator_triangulation.util.ChatUtils;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+
+public class Keybinds {
+    public static KeyBinding setPosKey;
+    private static boolean wasPressed = false;
+    public static boolean last_first_position = false;
+
+    public static void register() {
+        Keybinds.setPosKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            "key.locator_triangulation.set_pos",
+            InputUtil.Type.KEYSYM,
+            GLFW.GLFW_KEY_UNKNOWN,
+            "category.locator_triangulation"
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register((MinecraftClient client) -> {
+            if (client.player == null) {
+                return;
+            }
+            boolean isPressed = Keybinds.setPosKey.isPressed();
+
+            if (Keybinds.wasPressed && !isPressed) {
+                long windowHandle = client.getWindow().getHandle();
+                boolean shiftDown = (
+                    GLFW.glfwGetKey(windowHandle, GLFW.GLFW_KEY_LEFT_SHIFT) == GLFW.GLFW_PRESS
+                    || GLFW.glfwGetKey(windowHandle, GLFW.GLFW_KEY_RIGHT_SHIFT) == GLFW.GLFW_PRESS
+                );
+
+                if (shiftDown) {
+                    Keybinds.onShiftRelease();
+                } else {
+                    Keybinds.onRelease(client);
+                }
+            }
+
+            Keybinds.wasPressed = isPressed;
+        });
+    }
+
+    private static void onRelease(MinecraftClient client) {
+        LocatorInfo info = LocatorDataProvider.getLocatorInfo(client);
+        if (info == null) {
+            ChatUtils.sendErrorMessage("Failed to capture pos");
+        } else {
+            if (Keybinds.last_first_position) {
+                LocatorState.setPos2(info);
+                ChatUtils.sendConfirmationMessage("Locator pos2 saved");
+            } else {
+                LocatorState.setPos1(info);
+                ChatUtils.sendConfirmationMessage("Locator pos1 saved");
+            }
+            Keybinds.last_first_position = !Keybinds.last_first_position;
+        }
+    }
+
+    private static void onShiftRelease() {
+        ChatUtils.sendConfirmationMessage("Skipped saving locator position " + (Keybinds.last_first_position?2:1));
+        Keybinds.last_first_position = !Keybinds.last_first_position;
+    }
+}
